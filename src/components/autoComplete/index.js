b8e65c868ec0ebc61dbc694c3e60e34aab2cd9ca
shivam shrivastava <shivam.shrivastava1006@gmail.com>
2024-03-14 12:26:13 +0530

    diff b/w value & input value

--- src/components/autoComplete/index.js
+++ src/components/autoComplete/index.js
@@ -1,11 +1,31 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { top100Films } from '../../lib/top100films';
 
 export default function AutocompleteDemo() {
+
+    // *State for the selected value (actual value - from the provided options)
+    const [selectedValue, setSelectedValue] = useState(null);
+
+    // *State for the input value (user typed input - may or may not match an option)
+    const [inputText, setInputText] = useState('');
+
+    // *Handler for when an option is selected (user selected option)
+    const handleSelect = (value) => {
+        setSelectedValue(value);
+        console.log("User selected option", value);
+    };
+
+    // *Handler for when the input value changes (user typed values)
+    const handleInputChange = (event, value) => {
+        setInputText(value);
+        console.log("User typed input", value);
+    };
+
     return (
         <Autocomplete
+            // *Added Props //
             defaultValue={top100Films[0]}
             disableClearable
             autoComplete
@@ -13,9 +33,17 @@ export default function AutocompleteDemo() {
             autoSelect
             autoHighlight
             clearOnBlur
+
+            // *Added id & style //
             id="combo-box-demo"
-            options={top100Films}
             sx={{ width: 300 }}
+
+            // *Added options & handlers //
+            options={top100Films}
+            value={selectedValue}
+            onChange={(event, value) => handleSelect(value)}
+            inputValue={inputText}
+            onInputChange={(event, value) => handleInputChange(event, value)}
             renderInput={(params) => <TextField {...params} label="Movie" />}
         />
     );
